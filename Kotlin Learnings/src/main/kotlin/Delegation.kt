1b6eeb2603fa6256a0442c7794c72df79a6fb0c6
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-08-20 15:56:27 +0530

    Update Delegation.kt

--- Kotlin Learnings/src/main/kotlin/Delegation.kt
+++ Kotlin Learnings/src/main/kotlin/Delegation.kt
@@ -17,3 +17,19 @@ fun main(){
     val b = EmployeeImpl("Tech")
     println(EmployeeDerived(b).getDept())
 }
+
+// import kotlin.properties.Delegates
+
+// class User {
+//     // Define a property that will be observed
+//     var name: String by Delegates.observable("John") { _, oldName, newName ->
+//         println("Name changed from $oldName to $newName")
+//     }
+// }
+
+// fun main() {
+//     val user = User()
+    
+//     user.name = "Alice" // This will trigger the observable delegate and print the change.
+//     user.name = "Bob"   // This will trigger the observable delegate again.
+// }
